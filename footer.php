06f205ca2fb17456460d659801bb3a82ff8ada5d
gideon <dongidomed@gmail.com>
2016-11-24 23:32:27 +0100

    Wip: site development, remove preloader etc

---
+++ footer.php
@@ -0,0 +1,88 @@
+
+    <!-- ::::::::::::::::::::::::::: Footer Area Start ::::::::::::::::::::::::::: -->
+    <footer class="footer_area section_padding_60">
+        <!-- Bottom Footer Area Start -->
+        <div class="footer_bottom_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="footer_social_area">
+                            <a class="btn btn-default" href="#" role="button"><i class="icofont icofont-social-facebook"></i></a>
+                            <a class="btn btn-default" href="#" role="button"><i class="icofont icofont-social-google-plus"></i></a>
+                            <a class="btn btn-default" href="#" role="button"><i class="icofont icofont-social-skype"></i></a>
+                            <a class="btn btn-default" href="#" role="button"><i class="icofont icofont-social-twitter"></i></a>
+                            <a class="btn btn-default" href="#" role="button"><i class="icofont icofont-social-linkedin"></i></a>
+                        </div>
+                        <div class="footer_bottom">
+                            <p>Made with <i class="icofont icofont-heart-alt"></i> by <a href="#">DEV OF FUTURE</a></p>
+                        </div>
+                        <!-- Bottom Footer Copywrite Text Area End -->
+                    </div>
+                </div>
+                <!-- end./ row -->
+            </div>
+            <!-- end./ container -->
+        </div>
+        <!-- Bottom Footer Area End -->
+    </footer>
+    <!-- ::::::::::::::::::::::::::: Footer Area End ::::::::::::::::::::::::::: -->
+
+    <!-- ::::::::::::::::::::::::::: All jQuery Plugins ::::::::::::::::::::::::::: -->
+
+    <!-- jQuery (necessary for all JavaScript plugins) -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Waypoint js -->
+    <script src="js/jquery.waypoints.min.js"></script>
+
+    <!-- Owl-carousel js -->
+    <script src="js/owl.carousel.min.js"></script>
+
+    <!-- Ajax Contact js -->
+    <script src="js/ajax-contact.js"></script>
+
+    <!-- Meanmenu js -->
+    <script src="js/meanmenu.js"></script>
+
+    <!-- Onepage Nav js -->
+    <script src="js/jquery.nav.min.js"></script>
+
+    <!-- Magnific Popup js -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+
+    <!-- Counterup js -->
+    <script src="js/counterup.min.js"></script>
+
+    <!-- jQuery easing js -->
+    <script src="js/jquery.easing.1.3.js"></script>
+
+    <!-- Back to top js -->
+    <script src="js/jquery.scrollUp.js"></script>
+
+    <!-- Sticky js -->
+    <script src="js/jquery.sticky.js"></script>
+
+    <!-- MatchHeight js -->
+    <script src="js/jquery.matchHeight-min.js"></script>
+
+    <!-- YouTube Video Player js -->
+    <script src="js/YTPlayer.min.js"></script>
+
+    <!-- Parallax js -->
+    <script src="js/stellar.min.js"></script>
+
+    <!-- GOOGLE MAPS -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDk9KNSL1jTv4MY9Pza6w8DJkpI_nHyCnk" type="text/javascript"></script>
+
+    <!-- GOOGLE ACTIVE JS -->
+    <script src="js/map-active.js"></script>
+
+    <!-- Active js -->
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title  -->
+    <title>DOF - Home Of Developers</title>
+
+    <!-- Favicon  -->
+    <link rel="shortcut icon" href="img/core-img/favicon.ico">
+
+    <!-- ::::::::::::::::::::::::::: All CSS Files ::::::::::::::::::::::::::: -->
+
+    <!-- Style css -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <!--[if IE]>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+    <!-- Preloader Start -->
+    <!--<div id="preloader">
+        <div class="wrapper">
+            <div class="cssload-loader"></div>
+        </div>
+    </div> -->
+    <!-- /.end preloader -->
+
+    <!-- ::::::::::::::::::::::::::: Header Start ::::::::::::::::::::::::::: -->
+    <header class="header_area">
+        <!-- Main Header Area Start -->
+        <div class="main_header_area" id="sticky">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-sm-2 col-xs-9">
+                        <!-- Logo Area:: For better view in all device please use logo image max-width 70px -->
+                        <div class="logo_area">
+                            <a href="index.html"><img src="img/core-img/logo.png" alt=""></a>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-10 col-xs-12">
+                        <!-- Menu Area Start -->
+                        <div class="main_menu_area">
+                            <div class="mainmenu">
+                                <nav>
+                                    <ul id="nav">
+                                        <li class="current_page_item"><a href="#home">Home</a></li>
+                                        <li><a href="#about">About</a></li>
+                                        <li><a href="#service">Service</a></li>
+                                        <li><a href="#advisor">Advisor</a></li>
+                                        <li><a href="#work">Work</a></li>
+                                        <li><a href="#price">Prices</a></li>
+                                        <li><a href="#blog">Blog</a></li>
+                                        <li><a href="#contact">Contact</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+
+                        </div>
+                        <!-- Menu Area End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Main Header Area End -->
+    </header>
+    <!-- ::::::::::::::::::::::::::: Header End ::::::::::::::::::::::::::: -->
