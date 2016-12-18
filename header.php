16c1152a3aed8503be36c94523d85d4757f6eeba
Gideon <dongidomed@gmail.com>
2016-12-18 19:46:01 +0100

    added the commented out code

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 * @author : Onwuka Gideon  <dongidomed@gmail.com> <dongido>
 * @Description: Sends email...
 */
-/* comented out by emma :p
+
    if($_SERVER['REQUEST_METHOD'] == "POST"){
     die("mail integration in progress...");
       $name    = $_POST[''];
@@ -13,7 +13,7 @@
       $subject = $_POST[''];
       $status = (mail()) ? "success" : 'fail' ;
    }
-*/
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
